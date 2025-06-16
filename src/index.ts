99d9399213fda4b0dca3a5d8cd70aabe5a665d91
David Gillies <daggillies@gmail.com>
2025-06-16 17:21:43 -0600

    Add cors and json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 // index.js
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const DEFAULT_PORT = '3000';
 const port = process.env.PORT ?? DEFAULT_PORT;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Express Skeleton index');
   console.log('Response sent');
 });
